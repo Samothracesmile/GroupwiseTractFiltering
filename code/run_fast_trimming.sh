a3fcf56e9591b8da3569bde3fc061a1608b5c4b0
Yihao Xia <yihaoxia@outlook.com>
2020-11-08 19:51:27 -0800

    Update run_fast_trimming.sh

--- code/run_fast_trimming.sh
+++ code/run_fast_trimming.sh
@@ -3,5 +3,5 @@
 python fast_trimming.py --base_dir ../data/cst \
                     --batch_size 100 --group_perc 1 --cache_flag 0 \
                     --cpu_num 4 --length_min_perc 0.8 --length_max_perc 0.01 --sigma 8 \
-                    --distance_termination 3 --res_dir ../data/cst/res_test_20201108_2
+                    --distance_termination 3 --res_dir ../data/cst/res_test
                     
